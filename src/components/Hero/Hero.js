489fe5e449b8ae47cbd5cfa2028f9a5830ea8bed
JosiahABrown <josiah.alen.brown@gmail.com>
2022-02-01 16:37:45 -0800

    updated hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,18 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hey, <br />
+        I'm Josiah Brown
+      </SectionTitle>
+      <SectionText>
+        I am a creator with skills in blockchain technology, web development, customer success, business operations, and photography. 
+      </SectionText>
+      <Button onClick={() => window.location = 'mailto:josiah.alen.brown@gmail.com'}>Contact Me</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
